6c32e744559aa5d4dac1758042dbd6307f88811e
Anzor <48889137+anzorayubov@users.noreply.github.com>
2019-12-11 19:42:29 +0300

    feat: add logic for download fake posts

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {}
+
   title = 'my-app';
+  todos;
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(x =>{
+        console.log(x)
+        this.todos = x;
+      })
+  }
+
 }
